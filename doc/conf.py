dcd55c6e100da0660a7a470c02ead24869fc87e0
Russell Owen <rowen@uw.edu>
2019-01-08 10:59:55 -0800

    Initial files from the template kit
    and preliminary text for `README.rst`.

---
+++ doc/conf.py
@@ -0,0 +1,13 @@
+"""Sphinx configuration file for an LSST stack package.
+
+This configuration only affects single-package Sphinx documentation builds.
+"""
+
+from documenteer.sphinxconfig.stackconf import build_package_configs
+import lsst.ts.ATMCSSimulator
+
+
+_g = globals()
+_g.update(build_package_configs(
+    project_name='ts_ATMCSSimulator',
+    version=lsst.ts.ATMCSSimulator.version.__version__))
