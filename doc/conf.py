f4afee1542bf6c67e46a346b2c34c8f0f31e8eb1
Russell Owen <rowen@uw.edu>
2020-03-24 21:30:02 -0700

    Fix location of version information

--- doc/conf.py
+++ doc/conf.py
@@ -11,6 +11,6 @@ _g = globals()
 _g.update(
     build_package_configs(
         project_name="ts_ATMCSSimulator",
-        version=lsst.ts.ATMCSSimulator.version.__version__,
+        version=lsst.ts.ATMCSSimulator.__version__,
     )
 )
