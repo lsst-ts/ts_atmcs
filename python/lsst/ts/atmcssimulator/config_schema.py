83ca659bd5b98c5463bfe0a06574085702522adc
Wouter van Reeven <wvanreeven@lsst.org>
2023-08-28 19:30:55 +0200

    Make ATMCSCsc a Configurable CSC.

---
+++ python/lsst/ts/atmcssimulator/config_schema.py
@@ -0,0 +1,50 @@
+# This file is part of ts_atmcssimulator.
+#
+# Developed for the Vera Rubin Observatory Telescope and Site Systems.
+# This product includes software developed by the LSST Project
+# (https://www.lsst.org).
+# See the COPYRIGHT file at the top-level directory of this distribution
+# for details of code ownership.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+__all__ = ["CONFIG_SCHEMA"]
+
+import yaml
+
+CONFIG_SCHEMA = yaml.safe_load(
+    """
+    $schema: http://json-schema.org/draft-07/schema#
+    $id: https://github.com/lsst-ts/ts_atmcssimulator/blob/main/python/lsst/ts/atmcssimulator/config_schema.py
+    title: MTDome v1
+    description: Schema for ATMCS CSC configuration files.
+    type: object
+    properties:
+      host:
+        description: IP address of the TCP/IP interface.
+        type: string
+        format: hostname
+      cmd_evt_port:
+        description: Port number of the command and event TCP/IP interface.
+        type: integer
+      telemetry_port:
+        description: Port number of the telemetry TCP/IP interface.
+        type: integer
+    required:
+      - host
+      - cmd_evt_port
+      - telemetry_port
+    additionalProperties: false
+    """
+)
