997eb7dd1d9a59e26b6db1fb68f370dd9ed5c411
Wouter van Reeven <wvanreeven@lsst.org>
2024-04-19 18:22:28 +0200

    Fix doc build.

--- doc/conf.py
+++ doc/conf.py
@@ -3,14 +3,13 @@
 This configuration only affects single-package Sphinx documentation builds.
 """
 
-import lsst.ts.salobj  # noqa
+import lsst.ts.atmcssimulator  # noqa
 from documenteer.conf.pipelinespkg import *  # type: ignore # noqa
 
 project = "ts_atmcssimulator"
 html_theme_options["logotext"] = project  # type: ignore # noqa
 html_title = project
 html_short_title = project
-doxylink = {}  # Avoid warning: Could not find tag file _doxygen/doxygen.tag
 
 intersphinx_mapping["ts_salobj"] = ("https://ts-salobj.lsst.io", None)  # type: ignore # noqa
 intersphinx_mapping["ts_simactuators"] = ("https://ts-simactuators.lsst.io", None)  # type: ignore # noqa
